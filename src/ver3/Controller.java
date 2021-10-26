f2caf970c34a324494195ffc162f84162af360b0
Khanh Linh <khanhlinh.ph2002@gmail.com>
2021-10-26 17:56:11 +0700

    update GUI

--- src/ver3/Controller.java
+++ src/ver3/Controller.java
@@ -22,36 +22,25 @@ public class Controller implements Initializable {
   private ObservableList<String> items = FXCollections.observableArrayList();
   @FXML private TextField searchBar;
   @FXML private ListView<String> ListSearchWord = new ListView(items);
-  ArrayList<String> words = new ArrayList<>(
-          Arrays.asList("test", "dog","Human", "Days of our life", "The best day",
-                  "Friends", "Animal", "Human", "Humans", "Bear", "Life",
-                  "This is some text", "Words", "222", "Bird", "Dog", "A few words",
-                  "Subscribe!", "SoftwareEngineeringStudent", "You got this!!",
-                  "Super Human", "Super", "Like")
-  );
-  @FXML
-  void search(ActionEvent event) {
-    ListSearchWord.getItems().clear();
-    ListSearchWord.getItems().addAll(searchList(searchBar.getText(),words));
-  }
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    ListSearchWord.getItems().addAll(words);
-//    DatabaseConnection connection = new DatabaseConnection();
-//    Connection Conn = connection.getDBConnection();
-//    Statement stmt = null;
-//    ResultSet rs = null;
-//    try {
-//      stmt = Conn.createStatement();
-//      rs = stmt.executeQuery("SELECT word FROM tudienanhviet");
-//      while (rs.next()) {
-//        items.add(rs.getString(1));
-//      }
-//
-//      ListSearchWord.setItems(items);
-//    } catch (SQLException e) {
-//    }
+    DatabaseConnection connection = new DatabaseConnection();
+    Connection Conn = connection.getDBConnection();
+    Statement stmt = null;
+    ResultSet rs = null;
+    try {
+      stmt = Conn.createStatement();
+      rs = stmt.executeQuery("SELECT word FROM tudienanhviet");
+      int n = 0;
+      while (rs.next() && n < 200) {
+        items.add(rs.getString(1));
+        n++;
+      }
+
+      ListSearchWord.setItems(items);
+    } catch (SQLException e) {
+    }
   }
 
   private List<String> searchList(String searchWords, List<String> listOfStrings) {
