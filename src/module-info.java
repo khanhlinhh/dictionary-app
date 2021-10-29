cd3bf163c7756b4c8757a2c14bcfa2ec79f20a27
Khanh Linh <khanhlinh.ph2002@gmail.com>
2021-10-29 23:01:09 +0700

    di chuyển file

--- src/module-info.java
+++ src/module-info.java
@@ -1,10 +1,9 @@
 module BTL.OOP.INT2204_21 {
-        requires javafx.graphics;
-        requires javafx.controls;
-        requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
     requires java.sql;
     requires freetts;
-    requires com.jfoenix;
 
     opens ver3;
-        }
+}
