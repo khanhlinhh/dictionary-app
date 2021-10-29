9f350bc0c70623be25f137a23257f688cea8e7d3
Khanh Linh <khanhlinh.ph2002@gmail.com>
2021-10-29 23:02:04 +0700

    update search, show, sound func

--- src/ver3/Controller.java
+++ src/ver3/Controller.java
@@ -1,27 +1,63 @@
 package ver3;
 
-import com.jfoenix.controls.JFXListView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.ResourceBundle;
-import java.util.stream.Collectors;
 
 public class Controller implements Initializable {
+  DatabaseConnection connection = new DatabaseConnection();
+  Connection conn = connection.getDBConnection();
   @FXML
   private ObservableList<String> items = FXCollections.observableArrayList();
-  @FXML private TextField searchBar;
-  @FXML private ListView<String> ListSearchWord = new ListView(items);
+
+  @FXML
+  private ListView<String> ListSearchWord = new ListView(items);
+
+  @FXML
+  private TextArea meaningArea;
+
+  @FXML
+  private Label pronunLabel;
+
+  @FXML
+  private TextField searchBar;
+
+  @FXML
+  private Label wordLabel;
+
+  @FXML
+  void searchEnter(KeyEvent event) throws SQLException {
+    if (event.getCode() == KeyCode.ENTER) {
+      String word = searchBar.getText().toLowerCase();
+      String sql = "select *from tudienanhviet where word = ?";
+      PreparedStatement ps = conn.prepareStatement(sql);
+      ps.setString(1,word);
+      ResultSet rs = ps.executeQuery();
+      String meaning = rs.getString(3);
+      String pronun = rs.getString(4);
+      String wordSearch = rs.getString(1);
+      meaningArea.setText(meaning);
+      pronunLabel.setText(pronun);
+      wordLabel.setText(wordSearch);
+      TextToSpeech pronunSound = new TextToSpeech(wordSearch);
+    }
+  }
+
+  @FXML
+  void soundOnAction(ActionEvent event) {
+    String word = wordLabel.getText();
+    TextToSpeech pronunSearch = new TextToSpeech(word);
+  }
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
@@ -41,18 +77,5 @@ public class Controller implements Initializable {
       ListSearchWord.setItems(items);
     } catch (SQLException e) {
     }
-  }
-
-  private List<String> searchList(String searchWords, List<String> listOfStrings) {
-
-    List<String> searchWordsArray = Arrays.asList(searchWords.trim().split(" "));
-
-    return listOfStrings.stream()
-        .filter(
-            input -> {
-              return searchWordsArray.stream()
-                  .allMatch(word -> input.toLowerCase().contains(word.toLowerCase()));
-            })
-        .collect(Collectors.toList());
   }
 }
